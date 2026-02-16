c8fbd50a72482213aaa129cdca7996f5e2eab2ad
babludangi6266 <babludangi2000@gmail.com>
2026-02-16 20:49:34 +0530

    ....

--- server.js
+++ server.js
@@ -61,5 +61,5 @@ io.on('connection', (socket) => {
     console.log('User disconnected');
   });
 });
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5005;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
