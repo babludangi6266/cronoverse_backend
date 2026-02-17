b6e6e32003d5f1694212fbf5a3fbbcef33d6610d
babludangi6266 <babludangi2000@gmail.com>
2026-02-17 09:25:23 +0530

    Resove onbording issue

--- utils/emailService.js
+++ utils/emailService.js
@@ -2,18 +2,13 @@ const nodemailer = require('nodemailer');
 
 // Create the transporter using the settings that worked for you
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
+  host: 'smtp.resend.com',
   port: 587,
   secure: false,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: 'resend',
+    pass: process.env.RESEND_API_KEY, // Your Resend API key
   },
-  connectionTimeout: 10000, // 10 seconds timeout
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
-  // Force IPv4
-  family: 4, // Add this line to use only IPv4
 });
 const sendEmail = async (to, subject, html) => {
   try {
