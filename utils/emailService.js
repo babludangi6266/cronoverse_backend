cb5f8e70594ae7ab4033adc97e8deca8978603c6
babludangi6266 <babludangi2000@gmail.com>
2026-02-17 10:09:05 +0530

    ..

--- utils/emailService.js
+++ utils/emailService.js
@@ -5,8 +5,8 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendEmail = async (to, subject, html) => {
   try {
     const { data, error } = await resend.emails.send({
-      from: 'babludangi2000@gmail.com', // Default for testing; verify a domain for custom 'from'
-      to: [to],
+      from: 'Lexa Admin <no-reply@lexatechnologies.com>',  // Recommended: Use this or admin@lexatechnologies.com
+      to: [to],  // Can be a string or array
       subject: subject,
       html: html,
     });
